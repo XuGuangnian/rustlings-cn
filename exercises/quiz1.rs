9bfe1bb19d3a547bd8b29eb7b10beec0da5dbe0c
XuGuangnian <gengen.wang@outlook.com>
2023-11-02 00:41:34 +0800

    1-30 solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,14 @@
 // 写一个根据订单的苹果个数计算价格的函数。
 // 这次没有提示！
 
-
-// I AM NOT DONE
-
 // 将你的函数放在这里！
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 // 不要修改这个函数
 #[test]
